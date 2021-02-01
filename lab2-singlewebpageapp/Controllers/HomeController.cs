e072e7f593d77db461c2d6f18274d646f7a13f53
coolatent <62825067+coolatent@users.noreply.github.com>
2021-02-01 17:16:23 -0500

    lab2 tip calc complete

---
+++ lab2-singlewebpageapp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using lab2_singlewebpageapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab2_singlewebpageapp.Controllers
+{
+    public class HomeController : Controller // Traffic Cop
+    {
+        [HttpGet] // http get
+        public IActionResult Index()
+        {
+            ViewBag.Fifteen = 0; // envelope that can store attributes 
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+            return View();
+        }
+        [HttpPost] // http post
+
+        public IActionResult Index(TipCalculator tip) // forwarding user to another view
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.Fifteen = tip.CalculateTip(0.15);
+                ViewBag.Twenty = tip.CalculateTip(0.20);
+                ViewBag.TwentyFive = tip.CalculateTip(0.25);
+
+
+            }
+            else
+            {
+                ViewBag.Fifteen = 0; // envelope that can store attributes 
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+            return View(tip);
+        }
+
+    }
+}
---
+++ lab2-singlewebpageapp/Models/TipCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab2_singlewebpageapp.Models
+{
+    public class TipCalculator
+    {
+
+        [Required(ErrorMessage = "Please enter a value for the cost of your meal.")] // error message check
+        [Range(0.0, 10000.0, ErrorMessage = "Cost of meal must be greater than zero")]
+        // nullable attirbute "?" - ANY value including null, we put this here because first run sanity check for users
+        public double? MealCost { get; set; } // accessor and mutator : get and set
+
+        public double CalculateTip(double percent)
+        {
+            if (MealCost.HasValue)
+            {
+                var tip = MealCost.Value * percent;
+                return tip;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
